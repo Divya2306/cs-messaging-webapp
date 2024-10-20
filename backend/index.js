8e27682da85c28da75361873a0ceddf047fb6049
Divya2306 <96518035+Divya2306@users.noreply.github.com>
2024-10-21 00:44:47 +0530

    socket error fixes

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const messageRoutes = require('./routes/messageRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const messageRoutes = require("./routes/messageRoutes");
 const app = express();
 const http = require("http");
 const socketIo = require("socket.io");
 const server = http.createServer(app);
-const io = socketIo(server); // Initialize Socket.IO
+// const io = socketIo(server); // Initialize Socket.IO
+const io = new socketIo.Server(server, {
+  cors: {
+    origin: "*", // this was localhost3000 when running locally
+    methods: ["GET", "POST"],
+  },
+  maxHttpBufferSize: 1e8,
+});
 
-mongoose.connect('mongodb+srv://himanshuharsh:jiit0207@cluster0.m8ubevh.mongodb.net/messaging_app?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('Database connected'))
-.catch(err => console.error(err));
-
+mongoose
+  .connect(
+    "mongodb+srv://himanshuharsh:jiit0207@cluster0.m8ubevh.mongodb.net/messaging_app?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error(err));
 
 app.use(express.json());
-const corsOrigin ={
-    origin:'https://cs-messaging-web-app.netlify.app/', //or whatever port your frontend is using
-    credentials:true,            
-    optionSuccessStatus:200
-}
+const corsOrigin = {
+  origin: "*", //or whatever port your frontend is using
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOrigin));
-app.use('/api/messages', messageRoutes(io));
-
+app.use("/api/messages", messageRoutes(io));
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
